ccfe606990f171da9d493ab56c6751bb11bc5de0
saeki <87316126+gin-gali@users.noreply.github.com>
2021-11-24 18:24:35 +0900

    Vue Learning

--- Section10/vue3-test/src/views/CompositionTest.vue
+++ Section10/vue3-test/src/views/CompositionTest.vue
@@ -1,12 +1,18 @@
 <template>
   <div>Composition Test</div>
+  
 </template>
 
 <script>
 export default {
   setup() {
+    let name = 'Otani'
+    const age = 30
     console.log("setup");
-    console.log(this);
+    return {
+      name,
+      age 
+    }
   },
   created() {
     console.log("created");
