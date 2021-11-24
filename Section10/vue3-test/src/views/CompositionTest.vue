0d487a9422c77499b9d908d5e33a6478ad36a4f8
saeki <87316126+gin-gali@users.noreply.github.com>
2021-11-24 14:52:06 +0900

    Veturの警告を解決

---
+++ Section10/vue3-test/src/views/CompositionTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Composition Test</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log("setup");
+    console.log(this);
+  },
+  created() {
+    console.log("created");
+    console.log(this);
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
+</script>
+
+<style>
+</style>
