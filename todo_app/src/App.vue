eaff17fe9804db1d01ecd5eff35893f9ff90770e
saeki <87316126+gin-gali@users.noreply.github.com>
2022-01-12 17:15:56 +0900

    vite create

--- todo_app/src/App.vue
+++ todo_app/src/App.vue
@@ -1,3 +1,4 @@
+<!---
 <template>
   <div class="container">
     <Header />
@@ -16,6 +17,40 @@ export default {
     Header,
     Todos,
     addButton
+  }
+}
+</script>
+--->
+
+<template>
+  <div id="app">
+    <h1>Todo List</h1>
+    <input v-model="todo" /><br />
+    <button @click="addTodo">追加</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }} <span @click="removeTodo(index)">X</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.todo)
+      this.todo = ''
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
   }
 }
 </script>
