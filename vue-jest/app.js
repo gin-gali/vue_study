d5e75d29c2f9ea55c80ad6c5d1384c158e7481e3
sae <nyan_az@yahoo.co.jp>
2022-06-16 23:14:02 +0900

    jest練習

--- vue-jest/app.js
+++ vue-jest/app.js
@@ -1,21 +1,8 @@
+const toFizzBuzz = require('./modules/fizzbuzz')
+
 const app = () => {
   for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-      console.log('FizzBuzz!')
-      continue
-    }
-
-    if (i % 3 === 0) {
-      console.log('Fizz')
-      continue
-    }
-
-    if (i % 5 === 0) {
-      console.log('Buzz')
-      continue
-    }
-
-    console.log(i)
+    console.log(toFizzBuzz(i))
   }
 }
 
